41941d2f925be3e680bd0fdf8f02aeb282f12d64
Aastha Dixit <dixitaastha13@gmail.com>
2025-12-16 16:18:51 +0530

    Added productController.js with getProducts, getProductById, and createProduct functions with basic error handling

---
+++ controllers/productController.js
@@ -0,0 +1,18 @@
+const Product = require('../models/Product');
+
+exports.getProducts = async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+};
+
+exports.getProductById = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) return res.status(404).json({ message: 'Product not found' });
+
+  res.json(product);
+};
+
+exports.createProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(201).json(product);
+};
